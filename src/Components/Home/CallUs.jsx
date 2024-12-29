a278903890d533b4dee83eef4b45b62ed5136077
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-29 16:44:45 +0600

    call us done

---
+++ src/Components/Home/CallUs.jsx
@@ -0,0 +1,9 @@
+const CallUs = () => {
+    return (
+        <section className="bg-black py-24 mt-[130px]">
+            <p className="text-center font-Cinzel text-5xl font-bold text-white">Call Us: +88 0192345678910</p>
+        </section>
+    );
+};
+
+export default CallUs;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import BistroBoss from "../Components/Home/BistroBoss";
 import OurMenuCart from "../Components/Home/OurMenuCart";
 import OurMenu from "../Components/Home/OurMenu";
 import Testimonials from "../Components/Home/Testimonials";
+import CallUs from "../Components/Home/CallUs";
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <OrderOnline />
             <BistroBoss />
             <OurMenuCart />
+            <CallUs />
             <OurMenu />
             <Testimonials />
         </div>
