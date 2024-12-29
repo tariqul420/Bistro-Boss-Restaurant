085c6a28de23ee14ba1402abad7c237181f14736
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-29 12:37:00 +0600

    bistro boss section done

---
+++ src/Components/Home/BistroBoss.jsx
@@ -0,0 +1,24 @@
+import bg from "../../assets/home/chef-service.jpg";
+
+const BistroBoss = () => {
+    return (
+        <section
+            className="h-96 bg-fixed bg-cover bg-center w-10/12 mx-auto flex items-center justify-center"
+            style={{
+                backgroundImage: `url(${bg})`,
+            }}
+        >
+            <div className="space-y-2 bg-white text-black w-[85%] mx-auto px-[167px] py-12">
+                <h2 className="text-center font-Cinzel text-5xl font-semibold">Bistro Boss</h2>
+                <p className="text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Necessitatibus, libero accusamus laborum deserunt ratione
+                    dolor officiis praesentium! Deserunt magni aperiam dolor
+                    eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default BistroBoss;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Banner from "../Components/Home/Banner";
 import OrderOnline from "../Components/Home/OrderOnline";
+import BistroBoss from "../Components/Home/BistroBoss";
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <OrderOnline />
+            <BistroBoss />
         </div>
     );
 };
