3fcc7c005d62e7c7dcda67312260673d394f95a9
tariqul420 <tariqul.islam.programmer@gmail.com>
2025-01-02 10:27:55 +0600

    install query client and setup

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,17 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Toaster } from 'react-hot-toast'
 import AuthProvider from './Provider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster position='top-right' />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster position='top-right' />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
